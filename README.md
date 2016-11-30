2b523b7749275e826737fa4970f31618a1f87456
Olivier Pimi <pimium@gmail.com>
2016-11-30 03:20:01 +0100

    Comments are updated

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # sda5708_siemens
 LPC1114 library to drive the SDA5708 from Siemens
 using SPI0 
+
+check please [1] for more informations.
+
+[1] http://www.sbprojects.com/knowledge/footprints/sda5708/index.php
--- spi.c
+++ spi.c
@@ -18,12 +18,12 @@
 
  void init_SPI(void){
  //SET UP THE SPI
-    LPC_IOCON->PIO0_9          = (LPC_IOCON->PIO0_9 & ~(0x3)) | 0x1;   //set PIO2_3 up as MOSI function (sec. 7.4.34)
-    LPC_IOCON->PIO0_8          = (LPC_IOCON->PIO0_8 & ~(0x3)) | 0x1;   //set PIO2_2 up as MISO function (sec. 7.4.22)
-    LPC_IOCON->PIO0_2          = (LPC_IOCON->PIO0_2 & ~(0x3)) | 0x1;   //set PIO2_0 up as SSEL function (sec. 7.4.2)
-    LPC_IOCON->SWCLK_PIO0_10   = (LPC_IOCON->SWCLK_PIO0_10 & ~(0x3)) | 0x2;   //set PIO2_1 up as SCK function (sec. 7.4.9)
+    LPC_IOCON->PIO0_9          = (LPC_IOCON->PIO0_9 & ~(0x3)) | 0x1;   //set up as MOSI function (sec. 7.4.34)
+    LPC_IOCON->PIO0_8          = (LPC_IOCON->PIO0_8 & ~(0x3)) | 0x1;   //set up as MISO function
+    LPC_IOCON->PIO0_2          = (LPC_IOCON->PIO0_2 & ~(0x3)) | 0x1;   //set up as SSEL function
+    LPC_IOCON->SWCLK_PIO0_10   = (LPC_IOCON->SWCLK_PIO0_10 & ~(0x3)) | 0x2;   //set up as SCK function
     // LPC_IOCON->SCK_LOC  	   = (LPC_IOCON->SCK_LOC & ~(0x3));
-    LPC_SYSCON->SYSAHBCLKCTRL  |= ((1<<11));                             //enable clock to SPI1 block (sec. 3.5.14)
+    LPC_SYSCON->SYSAHBCLKCTRL  |= ((1<<11));                             //enable clock to SPI0 block (sec. 3.5.14)
     LPC_SYSCON->SSP0CLKDIV     |= 0xFF;                                //enable SPI clk by writing non-zero clk divisor (sec. 3.5.17)
     LPC_SYSCON->PRESETCTRL     |= 0x01;                                //clear SPI reset, SPI held in reset by default (sec. 3.5.2)
     LPC_SSP0->CR0              |= 0x07;                                //set for 8 bit transfer (sec. 14.6.1)
